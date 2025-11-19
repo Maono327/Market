699c0b5df3842a3438ab1f86b4c32b3e6f724e7a
Maono327 <maono3300@gmail.com>
2025-11-20 02:03:52 +0300

    change cart item and product services

--- src/main/java/com/maono/marketapplication/services/implementations/ProductServiceImpl.java
+++ src/main/java/com/maono/marketapplication/services/implementations/ProductServiceImpl.java
@@ -1,43 +1,90 @@
 package com.maono.marketapplication.services.implementations;
 
+import com.maono.marketapplication.models.CartItem;
 import com.maono.marketapplication.models.Product;
+import com.maono.marketapplication.repositories.CartItemRepository;
+import com.maono.marketapplication.repositories.util.Page;
 import com.maono.marketapplication.util.ProductSortType;
 import com.maono.marketapplication.repositories.ProductRepository;
 import com.maono.marketapplication.services.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
+    private final CartItemRepository cartItemRepository;
 
     @Override
-    public Page<Product> findByPage(String search, ProductSortType sort, int pageSize, int pageNumber) {
+    public Mono<Page<Product>> findByPage(String search, ProductSortType sort, int pageSize, int pageNumber) {
         String sortBy = switch (sort) {
             case ALPHA -> "title";
             case PRICE -> "price";
             case NO -> "";
         };
-        Pageable pageable = sortBy.isBlank() ?
-                PageRequest.of(pageNumber, pageSize) :
-                    PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).ascending());
+
+        Function<List<Product>, Mono<Page<Product>>> pageFunction = products ->
+                productRepository
+                        .totalCount()
+                        .flatMap(totalCount -> {
+                                        int totalPages = (int) Math.ceil((double) totalCount / pageSize);
+                                        return Mono.just(new Page<>(
+                                                products,
+                                                pageSize,
+                                                pageNumber,
+                                                pageNumber < totalPages,
+                                                pageNumber > 1,
+                                                totalPages)
+                                        );
+                                    }
+                                );
+
+        Function<List<Product>, Mono<List<Product>>> cartItemMapFunction = products -> {
+            List<Long> productsIds = products.stream().map(Product::getId).toList();
+            return cartItemRepository.findAllById(productsIds)
+                    .collectMap(CartItem::getId)
+                    .flatMap(mapped -> Flux.fromIterable(products.stream()
+                                    .peek(product -> {
+                                        if (mapped.containsKey(product.getId())) {
+                                            CartItem c = mapped.get(product.getId());
+                                            c.setProduct(product);
+                                            product.setCartItem(c);
+                                        }
+                                    })
+                                    .toList())
+                            .collectList());
+        };
+
+        int offset = (pageNumber - 1) * pageSize;
         if (search.isBlank()) {
-            return productRepository.findAll(pageable);
+            return productRepository.findProductsByPage(pageSize, offset, sortBy)
+                    .collectList()
+                    .flatMap(cartItemMapFunction)
+                    .flatMap(pageFunction);
         } else {
-            return productRepository.findProductByTitleContainingIgnoreCase(search, pageable);
+            return productRepository.findProductsByPageAndTitle(search, pageSize, offset, sortBy)
+                    .collectList()
+                    .flatMap(cartItemMapFunction)
+                    .flatMap(pageFunction);
         }
     }
 
     @Override
-    public Product findProductById(Long id) {
-        return productRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    public Mono<Product> findProductByIdWithRelations(Long id) {
+        return productRepository.findById(id)
+                .flatMap(product -> cartItemRepository.findById(product.getId())
+                        .map(cartItem -> {
+                            cartItem.setProduct(product);
+                            product.setCartItem(cartItem);
+                            return product;
+                        })
+                        .switchIfEmpty(Mono.just(product)));
     }
 
 }
