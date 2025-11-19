234b0d0f724aaca1c817c6b5f8c95f2affa535ba
Maono327 <maono3300@gmail.com>
2025-11-20 02:05:23 +0300

    change custom data filler

--- src/main/java/com/maono/marketapplication/util/CustomDataFiller.java
+++ src/main/java/com/maono/marketapplication/util/CustomDataFiller.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 
@@ -24,62 +25,60 @@ public class CustomDataFiller implements ApplicationRunner {
     public void run(ApplicationArguments args) {
         log.info("TEST DATA FILLING IS STARTED");
 
-        if (productRepository.findProductByTitle("Книга") == null) {
-            Product book = Product.builder()
-                    .title("Книга")
-                    .description("Интересная книга")
-                    .imageName("book.png")
-                    .price(BigDecimal.valueOf(699.99))
-                    .build();
-            productRepository.save(book);
-            log.warn("BOOK ADDED");
-        }
+        Product book = Product.builder()
+                .title("Книга")
+                .description("Интересная книга")
+                .imageName("book.png")
+                .price(BigDecimal.valueOf(699.99))
+                .build();
 
-        if (productRepository.findProductByTitle("Портфель") == null) {
-            Product briefcase = Product.builder()
-                    .title("Портфель")
-                    .description("Удобный и красивый портфель")
-                    .imageName("briefcase.png")
-                    .price(BigDecimal.valueOf(4000))
-                    .build();
-            productRepository.save(briefcase);
-            log.warn("BRIEFCASE ADDED");
-        }
+        Product briefcase = Product.builder()
+                .title("Портфель")
+                .description("Удобный и красивый портфель")
+                .imageName("briefcase.png")
+                .price(BigDecimal.valueOf(4000))
+                .build();
 
-        if (productRepository.findProductByTitle("Polaroid") == null) {
-            Product polaroid = Product.builder()
-                    .title("Polaroid")
-                    .description("Для крутых фотографий")
-                    .imageName("polaroid.png")
-                    .price(BigDecimal.valueOf(6599.99))
-                    .build();
-            productRepository.save(polaroid);
-            log.warn("POLAROID ADDED");
-        }
+        Product polaroid = Product.builder()
+                .title("Polaroid")
+                .description("Для крутых фотографий")
+                .imageName("polaroid.png")
+                .price(BigDecimal.valueOf(6599.99))
+                .build();
 
-        if (productRepository.findProductByTitle("Зонтик") == null) {
-            Product umbrella = Product.builder()
-                    .title("Зонтик")
-                    .description("Красивый и прозрачный зонт")
-                    .imageName("umbrella.png")
-                    .price(BigDecimal.valueOf(1999.99))
-                    .build();
-            productRepository.save(umbrella);
-            log.warn("UMBRELLA ADDED");
-        }
+        Product umbrella = Product.builder()
+                .title("Зонтик")
+                .description("Красивый и прозрачный зонт")
+                .imageName("umbrella.png")
+                .price(BigDecimal.valueOf(1999.99))
+                .build();
 
-        if (productRepository.findProductByTitle("Ваза") == null) {
-            Product vase = Product.builder()
-                    .title("Ваза")
-                    .description("Дизайнерская ваза")
-                    .imageName("vase.png")
-                    .price(BigDecimal.valueOf(12999.99))
-                    .build();
-            productRepository.save(vase);
-            log.warn("VASE ADDED");
-        }
+        Product vase = Product.builder()
+                .title("Ваза")
+                .description("Дизайнерская ваза")
+                .imageName("vase.png")
+                .price(BigDecimal.valueOf(12999.99))
+                .build();
 
 
-        log.info("TEST DATA FILLING IS FINISHED");
+        Mono<?> zip = Mono.zip(productRepository.findProductByTitleLike(book.getTitle())
+                        .switchIfEmpty(productRepository.save(book)
+                                .doOnSuccess(p -> log.info("Книга добавлена"))),
+                productRepository.findProductByTitleLike(briefcase.getTitle())
+                        .switchIfEmpty(productRepository.save(briefcase)
+                                .doOnSuccess(p -> log.info("Портфель добавлен"))),
+                productRepository.findProductByTitleLike(polaroid.getTitle())
+                        .switchIfEmpty(productRepository.save(polaroid)
+                                .doOnSuccess(p -> log.info("Полароид добавлен"))),
+                productRepository.findProductByTitleLike(umbrella.getTitle())
+                        .switchIfEmpty(productRepository.save(umbrella)
+                                .doOnSuccess(p -> log.info("Зонтик добавлен"))),
+                productRepository.findProductByTitleLike(vase.getTitle())
+                        .switchIfEmpty(productRepository.save(vase)
+                                .doOnSuccess(p -> log.info("Ваза добавлена")))
+                )
+                .doOnSuccess(p -> log.info("TEST DATA FILLING IS FINISHED"));
+
+        zip.subscribe();
     }
 }
