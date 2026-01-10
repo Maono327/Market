b0d4e5ef5f160f08a79f9680fdef37eb38b33c62
Maono327 <maono3300@gmail.com>
2026-01-10 17:49:57 +0300

    change ResetDataManager

--- showcase/src/test/java/com/maono/marketapplication/integration/ResetDataManager.java
+++ showcase/src/test/java/com/maono/marketapplication/integration/ResetDataManager.java
@@ -43,7 +43,32 @@ public class ResetDataManager {
     }
 
     public void resetAll() {
-        resetCartItems();
-        resetOrders();
+        String SQL =
+                """
+                TRUNCATE TABLE cart_items, orders, order_items, products RESTART IDENTITY;
+                
+                INSERT INTO products(title, description, image_name, price) VALUES ('Книга', 'Интересная книга', 'book.png', 699.99);
+                INSERT INTO products(title, description, image_name, price) VALUES ('Портфель', 'Удобный и красивый портфель', 'briefcase.png', 4000.00);
+                INSERT INTO products(title, description, image_name, price) VALUES ('Polaroid', 'Для крутых фотографий', 'polaroid.png', 6599.99);
+                INSERT INTO products(title, description, image_name, price) VALUES ('Зонтик', 'Красивый и прозрачный зонт', 'umbrella.png', 1999.99);
+                INSERT INTO products(title, description, image_name, price) VALUES ('Ваза', 'Дизайнерская ваза', 'vase.png', 12999.99);
+                
+                INSERT INTO cart_items(product_id, count) VALUES (1, 3);
+                INSERT INTO cart_items(product_id, count) VALUES (2, 1);
+                INSERT INTO cart_items(product_id, count) VALUES (5, 2);
+                
+                INSERT INTO orders(total_sum) VALUES (3399.97);
+                INSERT INTO orders(total_sum) VALUES (36699.93);
+                
+                INSERT INTO order_items(order_id, product_id, count) VALUES (1, 1, 2);
+                INSERT INTO order_items(order_id, product_id, count) VALUES (1, 4, 1);
+                
+                INSERT INTO order_items(order_id, product_id, count) VALUES (2, 1, 3);
+                INSERT INTO order_items(order_id, product_id, count) VALUES (2, 3, 1);
+                INSERT INTO order_items(order_id, product_id, count) VALUES (2, 4, 1);
+                INSERT INTO order_items(order_id, product_id, count) VALUES (2, 5, 2);
+                """;
+
+        r2dbcEntityTemplate.getDatabaseClient().sql(SQL).then().block();
     }
 }
