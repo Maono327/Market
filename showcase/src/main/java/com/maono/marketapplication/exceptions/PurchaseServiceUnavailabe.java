5a37cb28955df7470319c8b6eeced7eb631103d6
Maono327 <maono3300@gmail.com>
2026-01-19 12:06:45 +0300

    add exceptions

---
+++ showcase/src/main/java/com/maono/marketapplication/exceptions/PurchaseServiceUnavailabe.java
@@ -0,0 +1,7 @@
+package com.maono.marketapplication.exceptions;
+
+public class PurchaseServiceUnavailabe extends RuntimeException {
+    public PurchaseServiceUnavailabe(String message) {
+        super(message);
+    }
+}
